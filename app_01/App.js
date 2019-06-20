923d1443b3864070ce9ab7509adb014e08cc21b7
julianGonzalezV <j.andres2087@gmail.com>
2019-06-19 22:19:47 -0500

    study

--- app_01/App.js
+++ app_01/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default function App() {
+ state ={
+  placeName: ''
+ }
+
+ placeNameChangeHandler = val =>{
+	this.setState({
+		placeName: val	
+	});
+	
+ };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput 
+	style={{width: 300, borderColor:"black", borderWidth: 1}}
+	value={this.state.placeName} 
+	onChangeText={this.placeNameChangeHandler}/>
     </View>
   );
 }
---
+++ app_01/README.md
@@ -0,0 +1,6 @@
+# react_native_repo
+Learn (not develop) once deploy everywhere with react native 
+
+# install expo 
+
+# install react-native-cli
