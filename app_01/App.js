d60ab4bdc6b05c1a0b23c589286b8fd7db2d0040
julianGonzalezV <j.andres2087@gmail.com>
2019-06-25 22:50:36 -0500

    boton e input :)

--- app_01/App.js
+++ app_01/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
  state ={
-  placeName: ''
+  placeName: '',
+  places: []
  }
 
  placeNameChangeHandler = val =>{
@@ -13,12 +14,41 @@ export default function App() {
 	
  };
 
+ buttonClicHandler = val =>{
+	if(this.state.placeName.trim() === ""){
+    return;
+  }
+
+  this.setState(prevState => {
+    return{
+      places: prevState.places.concat(prevState.placeName)
+    }    
+  });
+	
+ };
+
+ render(){
+  const placesOut = this.state.places.map((place,index) => (
+    <Text key = {index}>
+      {place}
+    </Text>
+  ));
   return (
     <View style={styles.container}>
-      <TextInput 
-	style={{width: 300, borderColor:"black", borderWidth: 1}}
-	value={this.state.placeName} 
-	onChangeText={this.placeNameChangeHandler}/>
+      <View style={styles.inputButton}>
+        <TextInput 
+          style={{width: 300}}
+          value={this.state.placeName} 
+          onChangeText={this.placeNameChangeHandler}
+          style={styles.placeInput}/>
+        <Button
+            title="Add"
+            style={styles.placeButton}
+            onPress={this.buttonClicHandler}/>
+      </View>
+      <View>
+        placesOut
+      </View>
     </View>
   );
 }
@@ -26,8 +56,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 25,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  inputButton: {
+    width: "100%",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    alignItems:"center"
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
+  }
 });
