ab8445c4810375c97fe45bd89f08c6c8ba5cbdae
julianGonzalezV <j.andres2087@gmail.com>
2019-06-27 21:10:37 -0500

    ajuste código

--- app_01/App.js
+++ app_01/App.js
@@ -1,43 +1,51 @@
-import React from 'react';
+
+
+import React, {Component} from 'react';
 import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
-export default function App() {
- state ={
-  placeName: '',
-  places: []
- }
 
- placeNameChangeHandler = val =>{
-	this.setState({
-		placeName: val	
-	});
-	
- };
+export default class App extends Component {
 
- buttonClicHandler = val =>{
-	if(this.state.placeName.trim() === ""){
-    return;
-  }
 
-  this.setState(prevState => {
-    return{
-      places: prevState.places.concat(prevState.placeName)
-    }    
-  });
-	
- };
+  state ={
+    placeName: '',
+    places: []
+   }
 
- render(){
-  const placesOut = this.state.places.map((place,index) => (
-    <Text key = {index}>
-      {place}
-    </Text>
-  ));
-  return (
-    <View style={styles.container}>
+   placeNameChangeHandler = val =>{
+    this.setState({
+      placeName: val	
+    });
+    
+   };
+
+   buttonClicHandler = val =>{
+    if(this.state.placeName.trim() === ""){
+      return;
+    }
+  
+    this.setState(prevState => {
+      return{
+        places: prevState.places.concat(prevState.placeName)
+      }    
+    });
+    
+   };
+  
+
+
+  render() {
+    const placesOut = this.state.places.map((place,index) => (
+      <Text key = {index}>
+        {place}
+      </Text>
+    ));
+
+    return (
+      <View style={styles.container}>
       <View style={styles.inputButton}>
         <TextInput 
-          style={{width: 300}}
+          placeholder= "Ingrese los valores aquí ."
           value={this.state.placeName} 
           onChangeText={this.placeNameChangeHandler}
           style={styles.placeInput}/>
@@ -47,31 +55,34 @@ export default function App() {
             onPress={this.buttonClicHandler}/>
       </View>
       <View>
-        placesOut
+        {placesOut}
       </View>
     </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 25,
+    padding: 56,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
   inputButton: {
-    width: "100%",
-    flex: 1,
+   // flex: 1,
     flexDirection: "row",
     justifyContent: 'space-between',
     alignItems:"center"
   },
   placeInput: {
-    width: "70%"
+    width: "70%",
+    borderWidth:0,
+    borderRadius: 7
   },
   placeButton: {
     width: "30%"
   }
-});
+});  
+
